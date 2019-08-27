d6cb1c2a01c72999ad7b23f9d812b0988c4c72ca
chiboycalix <igwechinonso77@gmail.com>
2019-08-27 14:28:17 +0100

    feat(setup-e2e): Setup end-to-end-testing - Install nightwatch as a dependency - Install chromedriver dependency - Create nightwatch config file - Write tests [Finishes #167190388]

---
+++ src/components/TestComponent.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const TestComponent = () => <h1 data-test="first-test">Greetings from the commandos</h1>;
+
+export default TestComponent;
--- src/views/HomePage/index.js
+++ src/views/HomePage/index.js
@@ -4,9 +4,15 @@ import ArticleCard from '@Components/ArticleCard';
 
 const Home = () => (
   <div>
+<<<<<<< HEAD:src/views/HomePage/index.js
     <h1 data-test="homepageComponent">Home</h1>
     <Link to="/login">Click to Login</Link>
     <ArticleCard />
+=======
+    <h1>Home</h1>
+    <Link to="/login" data-test="login-link">Click to Login</Link>
+    <TestComponent />
+>>>>>>> feat(setup-e2e): Setup end-to-end-testing:src/views/Home.js
   </div>
 );
 
