1e317d0ba863ca666e2fe1bd53334ad0b64b2a93
cvjude <jjchinosoviolet@gmail.com>
2019-08-28 14:21:35 +0100

    Fix the the App extension in the src/index.js file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from '@Views/App.js';
+import App from './views/App.js';
 
 render(<App />, document.getElementById('root'));
