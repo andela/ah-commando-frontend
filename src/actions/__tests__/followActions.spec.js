1b27575343ca4dfe06058c794e388006979fa197
chiboycalix <igwechinonso77@gmail.com>
2019-09-13 13:50:47 +0100

    fixed rebasing issue

--- src/actions/__tests__/followActions.spec.js
+++ src/actions/__tests__/followActions.spec.js
@@ -10,7 +10,7 @@ import {
 const store = makeMockStore({});
 
 
-describe('testing redux actions', () => {
+describe('testing redux action', () => {
   beforeEach(() => {
     moxios.install();
   });
