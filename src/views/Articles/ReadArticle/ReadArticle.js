9e0a0249c00ea97a272dec66a1513521677cc460
chuxmykel <ngwobiachukwudi@gmail.com>
2019-09-13 14:12:51 +0100

    feat(articles): implement share articles feature
    - fix merge conflict
    
    [Delivers #167190404]

--- src/views/Articles/ReadArticle/ReadArticle.js
+++ src/views/Articles/ReadArticle/ReadArticle.js
@@ -149,6 +149,10 @@ export class ReadArticle extends Component {
       });
   }
 
+  handleClick = name => {
+    document.querySelector(`[aria-label="${name}"]`).click();
+  }
+
   handleTagClick = async (tag) => {
     const { getArticlesTag, history } = this.props;
     await getArticlesTag(tag, history);
